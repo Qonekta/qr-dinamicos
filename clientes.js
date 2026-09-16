3f28fb6a1b77428c727b1eaa71f628ddb87ecbe9
Qonekta <lattucalucas@gmail.com>
2026-09-16 18:43:39 -0300

    Actualizar clientes.js desde panel

--- clientes.js
+++ clientes.js
@@ -1,51 +1,65 @@
 const negocios = {
 
     "001": {
+
         nombre: "Barbería de prueba",
 
         botones: [
+
             {
                 nombre: "📱 WhatsApp",
                 enlace: "https://wa.me/549260XXXXXXXX"
             },
+
             {
                 nombre: "📸 Instagram",
                 enlace: "https://www.instagram.com/"
             },
+
             {
                 nombre: "⭐ Dejar una reseña",
                 enlace: "https://www.google.com/"
             },
+
             {
                 nombre: "📍 Cómo llegar",
                 enlace: "https://www.google.com/maps/"
-            }
+            },
+
         ]
+
     },
 
     "002": {
+
         nombre: "Restaurante de prueba",
 
         botones: [
+
             {
                 nombre: "📱 WhatsApp",
                 enlace: "https://wa.me/549260XXXXXXXX"
             },
+
             {
                 nombre: "📸 Instagram",
                 enlace: "https://www.instagram.com/"
             },
+
             {
                 nombre: "📖 Ver menú",
                 enlace: "https://www.google.com/"
             },
+
             {
                 nombre: "📍 Cómo llegar",
                 enlace: "https://www.google.com/maps/"
-            }
+            },
+
         ]
+
     },
-    
+
     "003": {
 
         nombre: "cafe san rafael",
@@ -70,11 +84,26 @@ const negocios = {
             {
                 nombre: "📍 Cómo llegar",
                 enlace: ""
-            }
+            },
 
         ]
 
     },
 
+    "004": {
+
+        nombre: "barberia san ra",
+
+        botones: [
+
+            {
+                nombre: "instagram",
+                enlace: "instagram.com"
+            },
+
+        ]
+
+    },
 
 };
+
