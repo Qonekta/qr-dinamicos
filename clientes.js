6955e3e78e751783f7f9c288c96ae48238a080ce
Qonekta <lattucalucas@gmail.com>
2026-09-24 21:18:10 -0300

    Actualizar clientes.js desde panel

--- clientes.js
+++ clientes.js
@@ -131,8 +131,8 @@ const negocios = {
         botones: [
 
             {
-                nombre: "a",
-                enlace: "https://a"
+                nombre: "⭐ Danos una opinión",
+                enlace: "https://search.google.com/local/writereview?placeid="
             },
 
         ]
